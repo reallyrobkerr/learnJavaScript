41e0f31800349359c40ddd609cee567a3295907c
Rob Kerr <reallyrobkerr@gmail.com>
2017-03-26 20:08:38 -0400

    add the styles and html with js

--- js/app.js
+++ js/app.js
@@ -37,7 +37,21 @@ $(document).ready(function() {
       console.log("keypress processed"); // write to the console to indicate something happened
       event.preventDefault(); // prevent page reload
       var newEntry = $(this).val(); // store the text input in a variable
-      $('.news').prepend(newEntry); // shove new items at the top of the list
+      $('.news').prepend('<li class=\'news-item\'> \ 
+  <a class=\'news-link\' href=\'#\'>' + newEntry + '</a> \
+  <ul class=\'news-meta\'> \
+    <li class=\'news-meta-item\'> \
+      <em>Posted by:</em> \
+      <a class=\'news-author js-show-modal\' href=\'#\'>Rob</a> \
+    </li> \
+    <li class=\'news-meta-item\'> \
+      <a href=\'#\'>0 Comments</a> \
+    </li> \
+    <li class=\'news-meta-item\'> \
+      <a class=\'js-like\' href=\'#\'>Do you like this?</a> \
+    </li> \
+  </ul> \
+</li>'); // shove new items at the top of the list, probably not the best way to do it
       $('.js-form').toggleClass('is-visible'); // hide the input field again
       $(this).val(null); // reset the input
     };
