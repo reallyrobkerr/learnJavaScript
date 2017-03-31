55d683ebe356ddb58421cdeb3382a1328d346e75
Rob Kerr <rkerr@igloosoftware.com>
2017-03-31 17:22:05 -0400

    move the html injection to global function

--- js/app.js
+++ js/app.js
@@ -1,77 +1,83 @@
-/*-----------------------------------------------
-    DOCUMENT READY
------------------------------------------------*/
+(function() {
+  function createListElement (newEntry, author){
+    var listElement = '<li class="news-item">';
+    listElement += '<a class="news-link" href="#">' + newEntry + '</a>';
+    listElement += '<ul class="news-meta"> ';
+    listElement += '<li class="news-meta-item"> ';
+    listElement += '<em>Posted by: </em> ';
+    listElement += '<a class="news-author js-show-modal" href="#">' + author + '</a>';
+    listElement += '</li> ';
+    listElement += '<li class="news-meta-item"> ';
+    listElement += '<a href="#">Comments</a> ';
+    listElement += '</li> ';
+    listElement += '<li class="news-meta-item"> ';
+    listElement += '<a class="js-like" href="#">Do you like this?</a> ';
+    listElement += '</li> ';
+    listElement += '</li>';
+    return listElement;
+  }
 
-$(document).ready(function() {
+  /*-----------------------------------------------
+      DOCUMENT READY
+  -----------------------------------------------*/
 
-  //-----------------------------------------------
-  //   Like
-  //-----------------------------------------------
-  
-  $('.news').on('click', '.js-like', function(event) {
-    event.preventDefault();
-    // ultra basic-bitch-newfag like/unlike toggle
-    if ($(this).text() === 'Do you like this?') {
-      $(this).text('Weirdo.')
-      .closest('.news-item')
-      .addClass('is-liked');
-    } else {
-      $(this).text('Do you like this?')
-      .closest('news-item')
-      .removeClass('is-liked')
-    };
+  $(document).ready(function() {
+
+    //-----------------------------------------------
+    //   Like
+    //-----------------------------------------------
+    
+    $('.news').on('click', '.js-like', function(event) {
+      event.preventDefault();
+      // ultra basic-bitch-newfag like/unlike toggle
+      if ($(this).text() === 'Do you like this?') {
+        $(this).text('Weirdo.')
+        .closest('.news-item')
+        .addClass('is-liked');
+      } else {
+        $(this).text('Do you like this?')
+        .closest('news-item')
+        .removeClass('is-liked')
+      };
+    });
+
+    //-----------------------------------------------
+    //   Add Link
+    //-----------------------------------------------
+
+    $('.js-add-link').on('click', function (event) {
+      event.preventDefault();
+
+      $('.js-form').toggleClass('is-visible');
+    });
+
+    $('#form-link-input').keypress(function (event){
+      if (event.which === 13) { // if enter is pressed in the form-link-input field
+        console.log("keypress processed"); // write to the console to indicate something happened
+        event.preventDefault(); // prevent page reload
+        var newEntry = $(this).val(); // store the text input in a variable
+        $('.news').prepend(createListElement(newEntry, 'author'));
+        $('.js-form').toggleClass('is-visible'); // hide the input field again
+        $('.news-brief').prepend('<li class="news-item"> \
+          <a class="news-link" href="#">' + newEntry + '</a>'); // add just the link to the modal
+        $(this).val(null); // reset the input
+      };
+    });
+
+    //-----------------------------------------------
+    //   Modal
+    //-----------------------------------------------
+
+    $('.news').on('click', '.js-show-modal', function (event) {
+      event.preventDefault();
+
+        $('.js-modal').addClass('is-visible');
+        $('.js-modal-overlay').addClass('is-visible');
+    });
+
+    $('.js-modal-overlay').on('click', function (event) {
+      $('.js-modal').removeClass('is-visible');
+      $('.js-modal-overlay').removeClass('is-visible');
+    });
   });
-
-  //-----------------------------------------------
-  //   Add Link
-  //-----------------------------------------------
-
-  $('.js-add-link').on('click', function (event) {
-    event.preventDefault();
-
-    $('.js-form').toggleClass('is-visible');
-  });
-
-  $('#form-link-input').keypress(function (event){
-    if (event.which === 13) { // if enter is pressed in the form-link-input field
-      console.log("keypress processed"); // write to the console to indicate something happened
-      event.preventDefault(); // prevent page reload
-      var newEntry = $(this).val(); // store the text input in a variable
-      $('.news').prepend('<li class=\'news-item\'> \
-  <a class=\'news-link\' href=\'#\'>' + newEntry + '</a> \
-  <ul class=\'news-meta\'> \
-    <li class=\'news-meta-item\'> \
-      <em>Posted by:</em> \
-      <a class=\'news-author js-show-modal\' href=\'#\'>Rob</a> \
-    </li> \
-    <li class=\'news-meta-item\'> \
-      <a href=\'#\'>0 Comments</a> \
-    </li> \
-    <li class=\'news-meta-item\'> \
-      <a class=\'js-like\' href=\'#\'>Do you like this?</a> \
-    </li> \
-  </ul> \
-</li>'); // shove new items at the top of the list, probably not the best way to do it
-      $('.js-form').toggleClass('is-visible'); // hide the input field again
-      $('.news-brief').prepend('<li class=\'news-item\'> \
-        <a class=\'news-link\' href=\'#\'>' + newEntry + '</a>'); // add just the link to the modal
-      $(this).val(null); // reset the input
-    };
-  });
-
-  //-----------------------------------------------
-  //   Modal
-  //-----------------------------------------------
-
-  $('.news').on('click', '.js-show-modal', function (event) {
-    event.preventDefault();
-
-      $('.js-modal').addClass('is-visible');
-      $('.js-modal-overlay').addClass('is-visible');
-  });
-
-  $('.js-modal-overlay').on('click', function (event) {
-    $('.js-modal').removeClass('is-visible');
-    $('.js-modal-overlay').removeClass('is-visible');
-  });
-});
+})();
